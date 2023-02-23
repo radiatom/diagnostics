9794a52bdd0094d3260243a5fd9c16a3af540d9a
radiatom <arsenmozol@gmail.com>
2023-02-23 18:55:44 +0200

    container-xxl in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,53 +6,52 @@ import { NavLink } from 'react-router-dom';
 
 
 const HederBootstrap = (props) => {
-      return (<div>
-
-            <nav className=" container-fluid navbar bg-dark bg-body-tertiary">
-                  <div className="container ">
-                        <div className="navbar-brand p-0 ">
-                              <span><NavLink to="/Home"><img src={logo} alt="logo" width="40" /></NavLink></span>
-                              <span className={`row row-cols-1 row-cols-md-3 row-cols-lg-3 ${s.navContact}`}>
-                                    <div>м.Луцьк</div>
-                                    <div>вул.Найкраща</div>
-                                    <div>тел. +38 055 55 55 555</div>
-                              </span>
-                              {/* <span className={`text-danger ${s.navbarSpan}`} >м.Луцьк вул.Найкраща тел. +38 055 55 55 555</span> */}
+      return (
+            <div>
+                  <nav className=" container-fluid navbar bg-dark bg-body-tertiary">
+                        <div className=" container">
+                              <div className=" container-xxl navbar-brand p-0 ">
+                                    <span><NavLink to="/Home"><img src={logo} alt="logo" width="40" /></NavLink></span>
+                                    <span className={` row row-cols-1 row-cols-md-3 row-cols-lg-3 ${s.navContact}`}>
+                                          <div class="col">Автосервіс</div>
+                                          <div class="col">м.Луцьк вул.Найкраща#</div>
+                                          <div class="col">тел. +38 055 55 55 555</div>
+                                    </span>
+                              </div>
                         </div>
-                  </div>
-            </nav>
-            <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-                  <div className="container">
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                              <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                              <ul className="navbar-nav">
-                                    <li className="nav-item">
-                                          <NavLink className="nav-link disabled" to="/Home">Про нас</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                          <NavLink className="nav-link disabled" to="/Services">Наші послуги</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                          <NavLink className="nav-link" to="/Diagnostics"><h6>Діагностика <span className="badge bg-secondary">New</span></h6></NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                          <NavLink className="nav-link disabled" to="/Articles">Корисні статті</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                          <NavLink className="nav-link disabled" to="/Gallery">Галерея</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                          <NavLink className="nav-link" to="/Сontacts">Контакти</NavLink>
-                                    </li>
-                              </ul>
+                  </nav>
+                  <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+                        <div className="container">
+                              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span className="navbar-toggler-icon"></span>
+                              </button>
+                              <div className="collapse navbar-collapse" id="navbarNav">
+                                    <ul className="navbar-nav">
+                                          <li className="nav-item">
+                                                <NavLink className="nav-link disabled" to="/Home">Про нас</NavLink>
+                                          </li>
+                                          <li className="nav-item">
+                                                <NavLink className="nav-link disabled" to="/Services">Наші послуги</NavLink>
+                                          </li>
+                                          <li className="nav-item">
+                                                <NavLink className="nav-link" to="/Diagnostics"><h6>Діагностика <span className="badge bg-secondary">New</span></h6></NavLink>
+                                          </li>
+                                          <li className="nav-item">
+                                                <NavLink className="nav-link disabled" to="/Articles">Корисні статті</NavLink>
+                                          </li>
+                                          <li className="nav-item">
+                                                <NavLink className="nav-link disabled" to="/Gallery">Галерея</NavLink>
+                                          </li>
+                                          <li className="nav-item">
+                                                <NavLink className="nav-link" to="/Сontacts">Контакти</NavLink>
+                                          </li>
+                                    </ul>
+                              </div>
                         </div>
-                  </div>
 
-            </nav>
-      </div>
+                  </nav>
+            </div>
       );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
-  // HashRouter,
-  BrowserRouter
+  HashRouter,
+  // BrowserRouter
 } from 'react-router-dom';
 import store from './reactRedux/redux';
 import { Provider } from 'react-redux';
@@ -16,20 +16,20 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <HashRouter>
-  //   <Provider store={store}>
-  //     <React.StrictMode>
-  //       <App />
-  //     </React.StrictMode>
-  //   </Provider>
-  // </HashRouter>
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
+  // <BrowserRouter>
+  //   <Provider store={store}>
+  //     <React.StrictMode>
+  //       <App />
+  //     </React.StrictMode>
+  //   </Provider>
+  // </BrowserRouter>
 );
 reportWebVitals();
 
