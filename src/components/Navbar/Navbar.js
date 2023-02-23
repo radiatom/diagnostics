78f92959a94204cbd08522c46035bad0536ba7ed
radiatom <arsenmozol@gmail.com>
2023-02-23 16:20:18 +0200

    css style navcontact

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,11 +8,16 @@ import { NavLink } from 'react-router-dom';
 const HederBootstrap = (props) => {
       return (<div>
 
-            <nav className="navbar bg-dark bg-body-tertiary">
-                  <div className="container">
-                        <div className="navbar-brand">
-                              <NavLink className="navbar-brand p-0 " to="/Home"><img src={logo} alt="logo" width="40" /></NavLink>
-                              <span className={`text-danger ${s.navbarSpan}`} >м.Луцьк вул.Найкраща тел. +38 055 55 55 555</span>
+            <nav className=" container-fluid navbar bg-dark bg-body-tertiary">
+                  <div className="container ">
+                        <div className="navbar-brand p-0 ">
+                              <span><NavLink to="/Home"><img src={logo} alt="logo" width="40" /></NavLink></span>
+                              <span className={`row row-cols-1 row-cols-md-3 row-cols-lg-3 ${s.navContact}`}>
+                                    <div>м.Луцьк</div>
+                                    <div>вул.Найкраща</div>
+                                    <div>тел. +38 055 55 55 555</div>
+                              </span>
+                              {/* <span className={`text-danger ${s.navbarSpan}`} >м.Луцьк вул.Найкраща тел. +38 055 55 55 555</span> */}
                         </div>
                   </div>
             </nav>
@@ -25,19 +30,19 @@ const HederBootstrap = (props) => {
                         <div className="collapse navbar-collapse" id="navbarNav">
                               <ul className="navbar-nav">
                                     <li className="nav-item">
-                                          <NavLink className="nav-link" to="/Home">Про нас</NavLink>
+                                          <NavLink className="nav-link disabled" to="/Home">Про нас</NavLink>
                                     </li>
                                     <li className="nav-item">
-                                          <NavLink className="nav-link" to="/Services">Наші послуги</NavLink>
+                                          <NavLink className="nav-link disabled" to="/Services">Наші послуги</NavLink>
                                     </li>
                                     <li className="nav-item">
                                           <NavLink className="nav-link" to="/Diagnostics"><h6>Діагностика <span className="badge bg-secondary">New</span></h6></NavLink>
                                     </li>
                                     <li className="nav-item">
-                                          <NavLink className="nav-link" to="/Articles">Корисні статті</NavLink>
+                                          <NavLink className="nav-link disabled" to="/Articles">Корисні статті</NavLink>
                                     </li>
                                     <li className="nav-item">
-                                          <NavLink className="nav-link" to="/Gallery">Галерея</NavLink>
+                                          <NavLink className="nav-link disabled" to="/Gallery">Галерея</NavLink>
                                     </li>
                                     <li className="nav-item">
                                           <NavLink className="nav-link" to="/Сontacts">Контакти</NavLink>
@@ -45,7 +50,7 @@ const HederBootstrap = (props) => {
                               </ul>
                         </div>
                   </div>
-                  
+
             </nav>
       </div>
       );
