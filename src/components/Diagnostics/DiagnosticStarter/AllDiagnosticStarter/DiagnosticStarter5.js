987314ecbff346a9238a3a8f401d5e4887b7b75b
radiatom <arsenmozol@gmail.com>
2023-03-15 16:09:30 +0200

    sorting out starter

--- src/components/Diagnostics/DiagnosticStarter/AllDiagnosticStarter/DiagnosticStarter5.js
+++ src/components/Diagnostics/DiagnosticStarter/AllDiagnosticStarter/DiagnosticStarter5.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import s from "./DiagnosticStarter.module.css"
 
 const DiagnosticStarter5 = (props) => {
@@ -7,13 +7,12 @@ const DiagnosticStarter5 = (props) => {
             <div className={`card m-1 ${s.card}`} >
                   <div className="card-body">
                         <h5 className="card-title">Діагностика статера</h5>
-                        <h6 className="card-subtitle mb-2 text-muted2">Час від часу стартер заклинює</h6>
-                        <p className="card-text">Діагностика в розробці</p>
+                        <h6 className="card-subtitle mb-2 text-muted2">Розбирання та дефектування стартера.</h6>
+                        <p className="card-text">Перевірка стану щіток, якоря, втулок, підшипників, бендикса, статора, втягуючого.</p>
                         <p className="card-text"></p>
                         <p className="card-text"></p>
                         <nav>
-                              <button onClick={() => props.getTestData(1.1)} className="btn btn-secondary">Почати діагностику</button>
-                              {/* <NavLink to="/Diagnostics/Starter/Bobbery/"><button onClick={() => props.getTestData(1.1)} className="btn btn-warning">Почати діагностику</button></NavLink> */}
+                              <NavLink to="/Diagnostics/Test/166"><button className={`btn btn-warning ${s.button}`}>Почати діагностику</button></NavLink>
                         </nav>
                   </div>
             </div>
