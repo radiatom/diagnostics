a1b2d6141d8d0d43f2e4c61bd151be05e54a6c3c
radiatom <arssenmozol@gmail.com>
2023-08-01 11:54:26 +0300

    fixed style

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -15,95 +15,88 @@ const Nav = ({ active, setActive, drop, setDrop }) => {
     return (
         <nav className={active ? "navbar navbar_active" : "navbar"}>
             <ul>
-                <li onClick={()=>setActive(false)}>
+                <li onClick={() => setActive(false)}>
                     <NavLink to="/Home">Про нас</NavLink>
                 </li>
-                <li onClick={()=>setActive(false)}>
+                <li onClick={() => setActive(false)}>
                     <NavLink to="/Home/services">Наші послуги</NavLink>
                 </li>
                 {/* <li onClick={()=>setActive(!active)}>
                     <NavLink to="/Gallery">Галерея сервера</NavLink>
                 </li> */}
                 <li>
-                    <button className="navbar__dropButton" onClick={()=>setDrop(!drop)}>
-                        Діагностика <span className={`border ${color}`}>new</span>
-                        <span className={drop?'navbar__dropButton_downIcon active':"navbar__dropButton_downIcon"}></span>
+                    <button
+                        className="navbar__dropButton"
+                        onClick={() => setDrop(!drop)}
+                    >
+                        Діагностика{" "}
+                        <span className={`border ${color}`}>new</span>
+                        <span
+                            className={
+                                drop
+                                    ? "navbar__dropButton_downIcon active"
+                                    : "navbar__dropButton_downIcon"
+                            }
+                        ></span>
                     </button>
                     <ul
                         className={drop ? "navList  open" : "navList"}
-                        onMouseLeave={()=>setDrop(!drop)}
+                        onMouseLeave={() => setDrop(!drop)}
                     >
                         <li
                             onClick={() => {
                                 setDrop(!drop);
-                                setActive(false)
+                                setActive(false);
                             }}
                         >
-                            <NavLink
-                                className="dropdown-item"
-                                to="/Diagnostics/Starter"
-                            >
-                                Стартер
-                            </NavLink>
+                            <NavLink to="/Diagnostics/Starter">Стартер</NavLink>
                         </li>
                         <li
                             onClick={() => {
                                 setDrop(!drop);
-                                setActive(false)
+                                setActive(false);
                             }}
                         >
-                            <NavLink
-                                className="dropdown-item"
-                                to="/Diagnostics/Altenator"
-                            >
+                            <NavLink to="/Diagnostics/Altenator">
                                 Генератор
                             </NavLink>
                         </li>
                         <li
                             onClick={() => {
                                 setDrop(!drop);
-                                setActive(false)
+                                setActive(false);
                             }}
                         >
-                            <NavLink
-                                className="dropdown-item"
-                                to="/Diagnostics/Battery"
-                            >
+                            <NavLink to="/Diagnostics/Battery">
                                 Акумулятор
                             </NavLink>
                         </li>
                         <li
                             onClick={() => {
                                 setDrop(!drop);
-                                setActive(false)
+                                setActive(false);
                             }}
                         >
-                            <NavLink
-                                className="dropdown-item"
-                                to="/Diagnostics/All"
-                            >
+                            <NavLink to="/Diagnostics/All">
                                 Всі види діагностики
                             </NavLink>
                         </li>
                         <li
                             onClick={() => {
                                 setDrop(!drop);
-                                setActive(false)
+                                setActive(false);
                             }}
                         >
-                            <NavLink
-                                className="dropdown-item"
-                                to="/Diagnostics/Test/7"
-                            >
+                            <NavLink to="/Diagnostics/Test/7">
                                 Результат діагностики
                             </NavLink>
                         </li>
                     </ul>
                 </li>
-                <li onClick={()=>setActive(false)}>
+                <li onClick={() => setActive(false)}>
                     <NavLink to="/Home/contact">Контакти</NavLink>
                 </li>
-                <li onClick={()=>setActive(false)}>
+                <li onClick={() => setActive(false)}>
                     <NavLink to="/Home/blogs">Корисні статті</NavLink>
                 </li>
             </ul>
