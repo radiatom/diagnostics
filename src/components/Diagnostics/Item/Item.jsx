6c78c6af379c4a651cdae40a3c7e1f361bad2e08
radiatom <arssenmozol@gmail.com>
2023-09-04 18:44:15 +0300

    corrected animation in items

--- src/components/Diagnostics/Item/Item.jsx
+++ src/components/Diagnostics/Item/Item.jsx
@@ -7,11 +7,34 @@ import { updateRes } from "./../TestsPage/Test/testReducer";
 const Item = ({ index, title, problem, text, link }) => {
     const dispatch = useDispatch();
     const [active, setActive] = useState(false);
+    const scrollHandler = (e) => {
+        const size =document.getElementById('item'+index).getBoundingClientRect().top-window.innerHeight
+        if (size <= 0) {
+            setActive(true);
+        } else {
+            setActive(false);
+        }
+    }; //реакція на скрол
     useEffect(() => {
-        setTimeout(() => setActive(true), index * 500);
+        const size =document.getElementById('item'+index).getBoundingClientRect().top-window.innerHeight
+        if (size <= 0) {
+            setActive(true);
+        } else {
+            setActive(false);
+        }
     }, []);
+    useEffect(() => {
+        document.addEventListener("scroll", scrollHandler);
+        return function cleanup() {
+            document.removeEventListener("scroll", scrollHandler);
+        };
+    }, []); //слідкувати за скролом
+    //getBoundingClientRect().top відстань від елемента до верхньої частини екрана 
+    //e.target.documentElement.scrollHeight Висота всього блоку в якому відслідковуємо скрол
+    //e.target.documentElement.scrollTop значення на якій висоті знаходиться скролл 0=в самому верху
+    //window.innerHeight висота екрану на пристрої який відкрив цю сторніку
     return (
-        <div className={active ? "Item active" : "Item"}>
+        <div className={active ? "Item active" : "Item"} id={"item"+index}>
             <h5>{title}</h5>
             <h6>{problem}</h6>
             <p>{text}</p>
