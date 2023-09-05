f3928cd89c9ba38563b16182848cecad6ac9b856
radiatom <arssenmozol@gmail.com>
2023-09-05 14:06:50 +0300

    install persist

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import 'react-slideshow-image/dist/styles.css'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "react-slideshow-image/dist/styles.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { PersistGate } from "redux-persist/integration/react";
 import {
-  // HashRouter,
-  BrowserRouter
-} from 'react-router-dom';
-import store from './redux/redux';
-import { Provider } from 'react-redux';
+    // HashRouter,
+    BrowserRouter,
+} from "react-router-dom";
+import store,{persistor} from "./redux/redux";
+import { Provider } from "react-redux";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <HashRouter>
-  //   <Provider store={store}>
-  //     <React.StrictMode>
-  //       <App />
-  //     </React.StrictMode>
-  //   </Provider>
-  // </HashRouter>
-  <BrowserRouter>
-    <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Provider>
-  </BrowserRouter>
+    // <HashRouter>
+    //   <Provider store={store}>
+    //     <React.StrictMode>
+    //       <App />
+    //     </React.StrictMode>
+    //   </Provider>
+    // </HashRouter>
+    <BrowserRouter>
+        <Provider store={store}>
+            <React.StrictMode>
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </React.StrictMode>
+        </Provider>
+    </BrowserRouter>
 );
 reportWebVitals();
-
