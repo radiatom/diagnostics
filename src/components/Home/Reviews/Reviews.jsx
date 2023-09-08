6bbf904732182a5c5d2219ccbcf1ff47fb3b9053
radiatom <arssenmozol@gmail.com>
2023-09-08 15:36:47 +0300

    added data reviews

---
+++ src/components/Home/Reviews/Reviews.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Reviews.scss'
+
+const Reviews = () => {
+    return (
+        <div className='reviews'>
+            
+        </div>
+    );
+}
+
+export default Reviews;
